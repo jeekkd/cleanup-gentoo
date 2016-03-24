b68fd5f225673c2e390aa936ec642a246b9bc24f
daulton <daulton@protonmail.com>
2016-03-23 23:14:10 -0400

    Added additional things to clean such as cache older then a week, checking system to see if its compliant with Gentoo Linux Security Advisories, etc

--- cleanup_gentoo.sh
+++ cleanup_gentoo.sh
@@ -6,7 +6,7 @@
 # uninstalling things or wanting to clean portage plus a couple extra things like 
 # emptying the trash and /var/tmp and /tmp
 
-echo "Cleaning /var/tmp and /tmp..."
+echo "* Cleaning /var/tmp and /tmp..."
 
 rm -rf /var/tmp/portage/*
 rm -rf /var/tmp/ccache/*
@@ -17,19 +17,31 @@ rm -rf /usr/portage/packages/*
 rm -rf /tmp/*
 rm -rf /var/cache/genkernel/*
 
-echo "Cleaning unused libraries and programs..."
+echo "* Cleaning cache that was accessed 7 days ago or moreS"
+
+find ~/.cache/ -type f -atime +7 | xargs rm -Rf
+
+echo "* Cleaning unused libraries and programs..."
 
 emerge -av --depclean
 emerge -cav
 
-echo "Cleaning out the trash..."
+echo "* Cleaning out the trash..."
 
 rm -rf /home/*/.local/share/Trash/*/** &> /dev/null
 rm -rf /root/.local/share/Trash/*/** &> /dev/null
 
-echo "Finishing up with checking for and rebuild missing libraries..."
+echo "* Checking system for compliance with Gentoo Linux Security Advisories"
+
+good_result="This system is not affected by any of the listed GLSAs"
+glsa_check_result=`glsa-check -t all`
+if [ "$(diff -q $glsa_check_result $good_result 2>&1)" = "" ] ; then
+    glsa-check -f all
+fi
+
+echo "* Finishing up with checking for and rebuild missing libraries..."
 
 revdep-rebuild -v
 
 echo
-echo "Complete!"
+echo "* Complete!"
