17502283d88775da51b502661231496a58c1b644
huuteml <daulton@protonmail.com>
2016-06-05 21:10:02 -0500

    Changed the purpose section to match the READMEs update, added extra cache cleaning, etc

--- cleanup_gentoo.sh
+++ cleanup_gentoo.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
-# Written by: https://github.com/turkgrb
+#!/usr/bin/env bash
+# Written by: https://gitlab.com/u/huuteml
 # Website: https://daulton.ca
 # Purpose: Gentoo cleaning script, intended to just be a simple script to run after
 # uninstalling things or wanting to clean portage plus a couple extra things like 
 # emptying the trash and /var/tmp and /tmp
 
 echo "Enter the user you wish to clean for: "
-read entered_user
+read -r entered_user
 
 echo "* Cleaning /var/tmp and /tmp..."
 rm -rf /var/tmp/portage/*
@@ -19,17 +19,17 @@ rm -rf /tmp/*
 rm -rf /var/cache/genkernel/*
 
 echo "* Cleaning cache that was accessed 7 days ago or more"
-find /home/$entered_user/.cache/ -type f -atime +7 | xargs rm -Rf
-find /home/$entered_user/.cache/google-chrome/Default/Cache/* -type f -atime +2 | xargs rm -Rf
-find /home/$entered_user/.cache/google-chrome/Default/Media\ Cache/* -type f -atime +2 | xargs rm -Rf
-find /home/$entered_user/.cache/google-chrome/Default/Local\ Storage/* -type f -atime +2 | xargs rm -Rf
-find /home/$entered_user/.cache/google-chrome/Default/IndexedDB/* -type f -atime +2 | xargs rm -Rf
-find /home/$entered_user/.cache/mozilla/firefox/*/cache2/entries/* -type f -atime +2 | xargs rm -Rf
-find /home/$entered_user/.cache/mozilla/firefox/*/thumbnails/* -type f -atime +2 | xargs rm -Rf
-find /home/$entered_user/.cache/fontconfig/* -type f -atime +2 | xargs rm -Rf
-find /home/$entered_user/.cache/keyring-*/* -type f -atime +2 | xargs rm -Rf
-find /home/$entered_user/.mozilla/firefox/Crash\ Reports/* -type f -atime +2 | xargs rm -Rf
-find /home/$entered_user/.thumbnails/normal/* -type f -atime +2 | xargs rm -Rf
+find /home/"$entered_user"/.cache/ -type f -atime +7 | xargs rm -Rf
+find /home/"$entered_user"/.cache/google-chrome/Default/Cache/* -type f -atime +2 | xargs rm -Rf
+find /home/"$entered_user"/.cache/google-chrome/Default/Media\ Cache/* -type f -atime +2 | xargs rm -Rf
+find /home/"$entered_user"/.cache/google-chrome/Default/Local\ Storage/* -type f -atime +2 | xargs rm -Rf
+find /home/"$entered_user"/.cache/google-chrome/Default/IndexedDB/* -type f -atime +2 | xargs rm -Rf
+find /home/"$entered_user"/.cache/mozilla/firefox/*/cache2/entries/* -type f -atime +2 | xargs rm -Rf
+find /home/"$entered_user"/.cache/mozilla/firefox/*/thumbnails/* -type f -atime +2 | xargs rm -Rf
+find /home/"$entered_user"/.cache/fontconfig/* -type f -atime +2 | xargs rm -Rf
+find /home/"$entered_user"/.cache/keyring-*/* -type f -atime +2 | xargs rm -Rf
+find /home/"$entered_user"/.mozilla/firefox/Crash\ Reports/* -type f -atime +2 | xargs rm -Rf
+find /home/"$entered_user".thumbnails/normal/* -type f -atime +2 | xargs rm -Rf
 
 echo "* Cleaning unused libraries and programs..."
 emerge -av --depclean
@@ -42,13 +42,13 @@ rm -rf /root/.local/share/Trash/*/** &> /dev/null
 echo "* Checking system for compliance with Gentoo Linux Security Advisories"
 
 good_result="This system is not affected by any of the listed GLSAs"
-glsa_check_result=`glsa-check -t all`
+glsa_check_result=$(glsa-check -t all)
 if [ "$(diff -q $glsa_check_result $good_result 2>&1)" = "" ] ; then
     glsa-check -f all
 fi
 
 echo "* Removing foreign man pages"
-find /usr/share/man -type d -not -name man*
+find /usr/share/man -type d -not -name "man*"
 
 echo "* Removing gtk docs"
 rm -rf /usr/share/gtk-doc
@@ -66,9 +66,9 @@ echo "Removing distfiles"
 rm /var/portage/distfiles/*
 
 echo "* Removing python compiled files and tests"
-find /usr/lib/python* -name \*.py? -delete
+find /usr/lib/python* -name "\*.py?" -delete
 find /usr/lib/python* -type d -name __pycache__ -delete
-find /usr/lib/portage -name \*.py? -delete
+find /usr/lib/portage -name "\*.py?" -delete
 find /usr/lib/portage -type d -name __pycache__ -delete
 rm -rf /usr/lib/python*/test
 
@@ -84,19 +84,15 @@ rm -rf /etc/config-archive
 
 isInstalledRotate=$(equery list "*" | grep logrotate)
 if [[ $isInstalledRotate = *[!\ ]* ]]; then
-	echo "* Logrotate is installed already, skipping"
+	echo "* Running a forced logrotate and cleaning logs"
+	logrotate --force /etc/logrotate.conf
+	find /var/log/ -name '*[0-5]*' -exec rm {} \;
 else
 	echo "* Would you like to install logrotate to keep your logs clean? YES/NO"
-	read logrotate_answer
+	read -r logrotate_answer
 	if [[ $logrotate_answer == "YES" || $logrotate_answer == "yes" ]] ; then
 		emerge app-admin/logrotate
 	fi
-fi
-
-if [[ $isInstalledRotate = *[!\ ]* ]]; then
-    echo "* Running a forced logrotate and cleaning logs"
-   logrotate --force /etc/logrotate.conf
-	find /var/log/ -name '*[0-5]*' -exec rm {} \;
 fi
 
 echo "* Finishing up with checking for and rebuild missing libraries..."
