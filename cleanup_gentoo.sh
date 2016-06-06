9a2eb30e411d27610071faccfa1b4c1cdbbdd7a7
huuteml <daulton@protonmail.com>
2016-06-05 21:29:06 -0500

    Changed the purpose section to match the READMEs update, added extra cache cleaning, etc

--- cleanup_gentoo.sh
+++ cleanup_gentoo.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # Written by: https://gitlab.com/u/huuteml
 # Website: https://daulton.ca
-# Purpose: Gentoo cleaning script, intended to just be a simple script to run after
-# uninstalling things or wanting to clean portage plus a couple extra things like 
-# emptying the trash and /var/tmp and /tmp
+# Purpose: To be a simple script to run after uninstalling packages to clean the dependencies or wanting to 
+# clean portages left over ebuilds from previous emerges. There is a couple extra handy things like emptying 
+# the trash, cleaning /var/tmp and /tmp, checks the system for compliance with Gentoo Linux Security 
+# Advisories and so forth.
 
-echo "Enter the user you wish to clean for: "
-read -r entered_user
+user=$(who am i | awk '{print $1}')
 
 echo "* Cleaning /var/tmp and /tmp..."
 rm -rf /var/tmp/portage/*
@@ -17,68 +17,61 @@ rm -rf /usr/portage/distfiles/*
 rm -rf /usr/portage/packages/*
 rm -rf /tmp/*
 rm -rf /var/cache/genkernel/*
+echo
 
-echo "* Cleaning cache that was accessed 7 days ago or more"
-find /home/"$entered_user"/.cache/ -type f -atime +7 | xargs rm -Rf
-find /home/"$entered_user"/.cache/google-chrome/Default/Cache/* -type f -atime +2 | xargs rm -Rf
-find /home/"$entered_user"/.cache/google-chrome/Default/Media\ Cache/* -type f -atime +2 | xargs rm -Rf
-find /home/"$entered_user"/.cache/google-chrome/Default/Local\ Storage/* -type f -atime +2 | xargs rm -Rf
-find /home/"$entered_user"/.cache/google-chrome/Default/IndexedDB/* -type f -atime +2 | xargs rm -Rf
-find /home/"$entered_user"/.cache/mozilla/firefox/*/cache2/entries/* -type f -atime +2 | xargs rm -Rf
-find /home/"$entered_user"/.cache/mozilla/firefox/*/thumbnails/* -type f -atime +2 | xargs rm -Rf
-find /home/"$entered_user"/.cache/fontconfig/* -type f -atime +2 | xargs rm -Rf
-find /home/"$entered_user"/.cache/keyring-*/* -type f -atime +2 | xargs rm -Rf
-find /home/"$entered_user"/.mozilla/firefox/Crash\ Reports/* -type f -atime +2 | xargs rm -Rf
-find /home/"$entered_user".thumbnails/normal/* -type f -atime +2 | xargs rm -Rf
+echo "* Cleaning browser and other caches..."
+rm -r  /home/$user/.cache/Chromium/Default/Cache/*
+rm /home/$user/.mozilla/firefox/*.default/*.sqlite /home/$user/.mozilla/firefox/*default/sessionstore.js
+rm -r /home/$user/.cache/mozilla/firefox/*.default/*
+echo
 
 echo "* Cleaning unused libraries and programs..."
 emerge -av --depclean
 emerge -cav
+echo
 
 echo "* Cleaning out the trash..."
 rm -rf /home/*/.local/share/Trash/*/** &> /dev/null
 rm -rf /root/.local/share/Trash/*/** &> /dev/null
+echo
 
 echo "* Checking system for compliance with Gentoo Linux Security Advisories"
-
 good_result="This system is not affected by any of the listed GLSAs"
 glsa_check_result=$(glsa-check -t all)
 if [ "$(diff -q $glsa_check_result $good_result 2>&1)" = "" ] ; then
     glsa-check -f all
 fi
+echo
 
 echo "* Removing foreign man pages"
 find /usr/share/man -type d -not -name "man*"
+echo
 
 echo "* Removing gtk docs"
 rm -rf /usr/share/gtk-doc
+echo
 
 echo "* Removing locale files"
 find /usr/share/locale/* -type d | xargs rm -rf
+echo
 
 echo "* Removing unused packages"
 eclean -d packages
+echo
 
 echo "* Checking for packages with changed use flags"
 emerge --ask --update --changed-use --deep @world
+echo
 
 echo "Removing distfiles"
 rm /var/portage/distfiles/*
-
-echo "* Removing python compiled files and tests"
-find /usr/lib/python* -name "\*.py?" -delete
-find /usr/lib/python* -type d -name __pycache__ -delete
-find /usr/lib/portage -name "\*.py?" -delete
-find /usr/lib/portage -type d -name __pycache__ -delete
-rm -rf /usr/lib/python*/test
+echo
 
 echo "* Disabling python bytecode generation"
 echo "PYTHONDONTWRITEBYTECODE=1" > /etc/env.d/99python
 env-update && source /etc/profile
 
-echo "* Removing binutils info pages"
-rm -rf binutils-data/*/*/info
-
+echo
 echo "* Removing config-archive"
 rm -rf /etc/config-archive
 
@@ -95,6 +88,7 @@ else
 	fi
 fi
 
+echo
 echo "* Finishing up with checking for and rebuild missing libraries..."
 revdep-rebuild -v
 
